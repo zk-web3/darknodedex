195a35f250d6cc82c8d3b6d634b36c0dd6fffdb7
Zeeshan Khan (ZK) <mohammadzeeshankhan347@gmail.com>
2025-06-14 18:27:43 +0530

    57

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import '../src/styles/globals.css';
-import { WagmiProvider, useAccount, useConnect, useSwitchNetwork } from 'wagmi';
+import { WagmiProvider, useAccount, useConnect, useSwitchChain } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { config } from '../src/utils/wallet'; // We will create this file next
 import { Toaster, toast } from 'react-hot-toast';
@@ -11,30 +11,32 @@ const queryClient = new QueryClient();
 const BASE_SEPOLIA_CHAIN_ID = 84532; // Base Sepolia Chain ID
 
 function MyApp({ Component, pageProps }) {
-  const { address, isConnected, chain } = useAccount();
-  const { connect } = useConnect();
-  const { switchNetwork } = useSwitchNetwork();
+  // Removed Wagmi hooks and handleConnectWallet from _app.jsx
+  // const { address, isConnected, chain } = useAccount();
+  // const { connect } = useConnect();
+  // const { switchChain } = useSwitchChain(); // Renamed from useSwitchNetwork
 
-  const handleConnectWallet = () => {
-      if (isConnected) {
-          if (chain?.id === BASE_SEPOLIA_CHAIN_ID) {
-              toast.success("You Are Already On Base Sepolia");
-          } else {
-              toast.error("Please connect to Base Sepolia Network.");
-              if (switchNetwork) {
-                  switchNetwork(BASE_SEPOLIA_CHAIN_ID);
-              }
-          }
-      } else {
-          connect({ connector: injected() });
-      }
-  };
+  // const handleConnectWallet = () => {
+  //     if (isConnected) {
+  //         if (chain?.id === BASE_SEPOLIA_CHAIN_ID) {
+  //             toast.success("You Are Already On Base Sepolia");
+  //         } else {
+  //             toast.error("Please connect to Base Sepolia Network.");
+  //             if (switchChain) {
+  //                 switchChain(BASE_SEPOLIA_CHAIN_ID);
+  //             }
+  //         }
+  //     } else {
+  //         connect({ connector: injected() });
+  //     }
+  // };
 
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <Layout handleConnectWallet={handleConnectWallet} isConnected={isConnected} address={address} chain={chain}>
-          <Component {...pageProps} isConnected={isConnected} address={address} chain={chain} handleConnectWallet={handleConnectWallet} />
+        {/* Pass no wallet-related props here; Layout will handle them */}
+        <Layout>
+          <Component {...pageProps} />
         </Layout>
         <Toaster />
       </QueryClientProvider>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,31 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { useAccount, useConnect, useSwitchChain } from 'wagmi';
+import { toast } from 'react-hot-toast';
+import { injected } from 'wagmi/connectors';
 
-const Layout = ({ children, isConnected, address, chain, handleConnectWallet }) => {
+const BASE_SEPOLIA_CHAIN_ID = 84532; // Base Sepolia Chain ID
+
+const Layout = ({ children }) => {
+    const { address, isConnected, chain } = useAccount();
+    const { connect } = useConnect();
+    const { switchChain } = useSwitchChain();
+
+    const handleConnectWallet = () => {
+        if (isConnected) {
+            if (chain?.id === BASE_SEPOLIA_CHAIN_ID) {
+                toast.success("You Are Already On Base Sepolia");
+            } else {
+                toast.error("Please connect to Base Sepolia Network.");
+                if (switchChain) {
+                    switchChain(BASE_SEPOLIA_CHAIN_ID);
+                }
+            }
+        } else {
+            connect({ connector: injected() });
+        }
+    };
+
     return (
         <div className="min-h-screen flex flex-col bg-gray-900 font-inter text-white">
             <Navbar isConnected={isConnected} address={address} chain={chain} handleConnectWallet={handleConnectWallet} />
